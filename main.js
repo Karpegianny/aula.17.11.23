77d91656be67f7cd95ba90fcc4641d4f3fb0c811
Karpgianny Ferreira <karpgianny.albuquerque@gmail.com>
2023-11-17 11:01:44 -0300

    comentario

--- main.js
+++ main.js
@@ -1,5 +1,27 @@
+import './style.css'
+
+const usuarios = [{
+  nome: "Danilo",
+  idade: 31,
+  profissao: "Estudante"
+},{
+  nome: "Fabio",
+  idade: 25,
+  profissao: "Programador"
+},{
+  nome: "Julia",
+  idade: 22,
+  profissao: "Enfermeira"
+}];
+
+/*const names = usuarios.map((usuario) => usuario.profissao)
+console.log(names)*/
+
+const usuarioFiltrado = usuarios.filter((usuario)=> usuario.profissao === "Estudante")
+console.log(usuarioFiltrado)
+
 function dadosAluno ( nome, idade, sexo){
   alert(`Meu nome é ${nome} tenho ${idade} anos e sou do sexo ${sexo}`)
 }
 
-dadosAluno("Karpgianny", 40, "Masculino")
+/*dadosAluno("Karpgianny", 40, "Masculino")*/
